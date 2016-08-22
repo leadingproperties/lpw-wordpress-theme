b682d6679648637a786b370e79215d3a93bb88a0
Alex Baidan <howtwizer@gmail.com>
2016-08-22 11:05:29 +0300

    update default page view

--- page.php
+++ page.php
@@ -1,4 +1,10 @@
+<?php
+  get_template_part('templates/head');
+  get_header();
+?>
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+<?php
+  get_footer();
