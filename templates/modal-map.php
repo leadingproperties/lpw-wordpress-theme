adf5cfc4ad033c8f7caf5a0ac9e88d2cbb6bb020
alevgal <evpatij76@gmail.com>
2016-08-23 22:35:58 +0300

    Add map modal template

---
+++ templates/modal-map.php
@@ -0,0 +1,9 @@
+<!-- Map Modal -->
+<div id="map-modal" class="modal modal-large" tabindex="-1" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <button type="button" class="btn btn-close modal-close" data-dismiss="modal" aria-label="Close">Close</button>
+            <div id="map-canvas" class="map-full"></div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
