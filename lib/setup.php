accb12c3e128b91e27fcfe827b984d1c884933aa
alevgal <evpatij76@gmail.com>
2017-03-07 16:39:19 +0200

    WP-140 fix search dropdown padding

--- lib/setup.php
+++ lib/setup.php
@@ -67,8 +67,8 @@ function lp_rewrite_rule(){
  */
 function assets() {
   global $lp_settings;
-  $ver = wp_get_theme()->get('Version');
-	print_r(wp_get_theme());
+  $ver = "3.1.1";
+	  //TODO Check why wp_get_theme()->get('Version'); not working
     if(!is_admin()) {
         wp_enqueue_style( 'lprop/css', Assets\asset_path( 'styles/main.css' ), false, $ver );
 
